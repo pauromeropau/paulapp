6edb9a53013d1fcdfc146846d3dc9c9f0b13f8fb
pauromeropau <pauromeropau@gmail.com>
2020-04-14 21:00:41 +0100

    first commit

---
+++ lib/src/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:paulapp/pages/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return MaterialApp(
+        home: Center(
+      child: HomePage(),
+    ));
+  }
+}
